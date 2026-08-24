bdc9cd68e96e7abe2e0ac24136f1aabb4bf2b9bc
kawer95 <1034765569@qq.com>
2026-08-24 15:20:32 +0800

    修复孢子接口 Mixin 启动崩溃

--- src/main/java/com/arxyt/territorycontrolcompat/mixin/SporeFoliageSpreadMixin.java
+++ src/main/java/com/arxyt/territorycontrolcompat/mixin/SporeFoliageSpreadMixin.java
@@ -16,13 +16,13 @@ import org.spongepowered.asm.mixin.injection.Redirect;
  */
 @Pseudo
 @Mixin(targets = "com.Harbinger.Spore.Sentities.FoliageSpread")
-public abstract class SporeFoliageSpreadMixin {
+public interface SporeFoliageSpreadMixin {
     @Redirect(
             method = "convertWood(Lnet/minecraft/world/level/Level;Lnet/minecraft/world/level/block/state/BlockState;Lnet/minecraft/core/BlockPos;)V",
             at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/item/FallingBlockEntity;m_201971_(Lnet/minecraft/world/level/Level;Lnet/minecraft/core/BlockPos;Lnet/minecraft/world/level/block/state/BlockState;)Lnet/minecraft/world/entity/item/FallingBlockEntity;"),
             remap = false,
             require = 0)
-    private static FallingBlockEntity territorycontrolcompat$guardFungalFallingConversion(
+    public static FallingBlockEntity territorycontrolcompat$guardFungalFallingConversion(
             Level level, BlockPos pos, BlockState state) {
         return SporeCompat.allowFungalInfectionFallingConversion(level, pos)
                 ? FallingBlockEntity.fall(level, pos, state)
